6f1b3e2109871388435b15100aa4cc44ba1ff45a
Rafal Furgala <rafal.furgala@altium.com>
2022-11-04 17:23:42 +0100

    Wykład 18 mapowanie modeli Dto

---
+++ RestaurantAPI/Program.cs
@@ -0,0 +1,41 @@
+﻿//utworzenie webhosta, wcześniej w program.cs
+using RestaurantAPI;
+using RestaurantAPI.Entities;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//configure services (wszystko z klasy startup, metoda configureSrvices). Wszedzie gdzie configuration => builder.Configuration, builder.Services. Automapper => Assembly.GetExecutingAssembly()
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<RestaurantDbContext>();
+builder.Services.AddScoped<RestaurantSeeder>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//wszystko z metody Configure. 
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+seeder.Seed();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapRazorPages();
+
+app.Run();
